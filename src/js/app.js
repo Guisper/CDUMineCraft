90dadc69c577e3ed685a01bba87a29f6f12d350f
Guisper <3038363597@qq.com>
2022-11-06 03:19:03 +0800

    更换swiper版本，基本完成第二页的开发

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,40 @@
-import '../style/style.less'
+import Swiper from './swiper-bundle.min'
+import '../style/swiper-bundle.min.css'
 import '../style/animate.min.css'
-console.log('first')
+
+import '../style/style.less'
+
+const swiper = new Swiper('.mySwiper', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+})
+
+new Swiper('.mySwiper2', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 2000,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
+  },
+  thumbs: {
+    swiper
+  },
+})
+
+const anchorLink = document.querySelector('.btn button')
+const target = document.querySelector('.box')
+
+anchorLink && anchorLink.addEventListener('click', function (e) {
+  if (window.scrollTo) {
+    e.preventDefault()
+    window.scrollTo({ 'behavior': 'smooth', 'top': target.offsetTop })
+  }
+})
