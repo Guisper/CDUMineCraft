559612589daecf69a4954532f80e68b527531642
Guisper <3038363597@qq.com>
2022-11-08 14:07:31 +0800

    更改部分样式和效果，更换图片源为图床，使用图片懒加载提升首屏渲染速度

--- src/js/swiper.ts
+++ src/js/swiper.ts
@@ -3,6 +3,9 @@ import { clickToSlide } from './anchor'
 
 // 页面整体竖向滚动
 const pageSilde = new Swiper('#page', {
+  lazy: {
+    loadPrevNext: true
+  },
   direction: 'vertical',
   slidesPerView: 1,
   spaceBetween: 30,
@@ -19,6 +22,9 @@ clickToSlide('.btn button', pageSilde)
 const menuButton = document.querySelector('.menu-button')
 const openMenu = () => (menuSwiper as any).slidePrev()
 const menuSwiper = new Swiper('#menu', {
+  lazy: {
+    loadPrevNext: true
+  },
   slidesPerView: 'auto',
   initialSlide: 1,
   resistanceRatio: 0,
@@ -45,6 +51,9 @@ const menuSwiper = new Swiper('#menu', {
 
 // 第二页
 const swiper = new Swiper('.mySwiper', {
+  lazy: {
+    loadPrevNext: true
+  },
   loop: true,
   spaceBetween: 10,
   slidesPerView: 4,
@@ -53,6 +62,9 @@ const swiper = new Swiper('.mySwiper', {
 })
 
 new Swiper('.show', {
+  lazy: {
+    loadPrevNext: true
+  },
   loop: true,
   spaceBetween: 10,
   navigation: {
@@ -71,6 +83,9 @@ new Swiper('.show', {
 
 // 第三页游戏实录的立方体
 new Swiper('.cube', {
+  lazy: {
+    loadPrevNext: true
+  },
   effect: 'cube',
   autoplay: {
     delay: 1500,
